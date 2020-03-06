d19d4140854ad922cf9a464e44faf2a381609108
isalfati <ivansalfatift@gmail.com>
2020-03-06 21:57:50 +0100

    Barcelona Update.

--- collectData.py
+++ collectData.py
@@ -1,16 +1,19 @@
+import sys
+import math
 import openaq
 import warnings
 import numpy as np
 import pandas as pd
+import geopy.distance
 # import seaborn as sns
-# import matplotlib as mpl
+import matplotlib as mpl
 from datetime import datetime
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from pygsp import graphs, filters, plotting
 
 # Global Variables
-city = "Madrid"
-selection = ['o3', 'no2', 'pm25']
+city = "Barcelona"
+selection = ['o3', 'no2', 'pm10'] #PM25 does not exist in Barcelona
 
 # Avoid warnings
 warnings.simplefilter(action='ignore')
@@ -19,6 +22,11 @@ warnings.simplefilter(action='ignore')
 pd.options.display.max_columns = None
 pd.options.display.max_rows = None
 pd.options.display.width = None
+
+plotting.BACKEND = 'matplotlib'
+plt.rcParams['figure.figsize'] = (10, 5)
+
+np.set_printoptions(threshold=sys.maxsize)
 
 # Check versions
 print("Versions of the libraries:\n")
@@ -30,8 +38,9 @@ print("Pandas v{}".format(pd.__version__))
 api = openaq.OpenAQ()
 
 # List of Parameters
-print("\nThis program gathers information of the following parameters:\n")
-#param = api.parameters(df=True)
+print("\nThis program gathers information of some of the following parameters:\n")
+param = api.parameters(df=True)
+print(param)
 print(selection)
 print()
 
@@ -40,8 +49,9 @@ date = datetime.now().strftime("%Y_%m_%d-%H_%M_%S")
 filename = "/home/zap2x/Desktop/GSP/Datasets/" + date
 
 # Obtaining the latest values of the sensors in a City
-resCity = api.latest(city=city, parameters=['o3', 'no2', 'pm25'], df=True)
-#resCityParams.to_csv(filename + "_all_params_" + city + ".csv", sep=',')
+resCity = api.latest(city=city, parameters=selection, df=True)
+#print(resCity)
+#resCity.to_csv(filename + "_all_params_" + city + ".csv", sep=',')
 
 basicData = pd.DataFrame(resCity[['location', 'parameter', 'value']])
 refinedData = basicData[basicData.parameter.str.contains('|'.join(selection))]
@@ -56,22 +66,89 @@ cityLocationsLeftJoin = cityLocations[['location', 'coordinates.longitude', 'coo
 
 mergedDataSet = pd.merge(left=refinedData, right=cityLocationsLeftJoin, how='left', left_on='location', right_on='location')
 #mergedDataSet.to_csv(filename + "_" + city + "_DataSet.csv", sep=',')
-print(mergedDataSet)
+#print(mergedDataSet)
 
 dataO3 = mergedDataSet[mergedDataSet.parameter.str.contains('o3')]
 dataO3.reset_index(drop = True, inplace = True)
 #dataO3.to_csv(filename + "_O3_" + city + ".csv", sep=',')
-print("\n============================== O3 DATA ==============================")
-print(dataO3)
+#print("\n============================== O3 DATA ==============================")
+#print(dataO3)
 
 dataNO2 = mergedDataSet[mergedDataSet.parameter.str.contains('no2')]
 dataNO2.reset_index(drop = True, inplace = True)
 #dataNO2.to_csv(filename + "_NO2_" + city + ".csv", sep=',')
-print("\n============================== NO2 DATA ==============================")
-print(dataNO2)
+#print("\n============================== NO2 DATA ==============================")
+#print(dataNO2)
 
-dataPM25 = mergedDataSet[mergedDataSet.parameter.str.contains('pm25')]
-dataPM25.reset_index(drop = True, inplace = True)
-#dataPM25.to_csv(filename + "_PM25_" + city + ".csv", sep=',')
-print("\n============================== PM25 DATA ==============================")
-print(dataPM25)
+dataPM10 = mergedDataSet[mergedDataSet.parameter.str.contains('pm10')]
+dataPM10.reset_index(drop = True, inplace = True)
+#dataPM10.to_csv(filename + "_PM10_" + city + ".csv", sep=',')
+#print("\n============================== PM25 DATA ==============================")
+#print(dataPM10)
+
+# Graph Creation O3
+pointsO3 = [dataO3['coordinates.longitude'].tolist(), dataO3['coordinates.latitude'].tolist()]
+#print("Coordinates O3")
+#print(pointsO3)
+
+sizeO3 = len(dataO3)
+WO3 = np.zeros((sizeO3, sizeO3))
+
+for i in range(0, len(pointsO3[0])):
+    pointTmp = [pointsO3[0][i], pointsO3[1][i]]
+    #print(*pointTmp, sep=", ")
+    for j in range(i, len(pointsO3[0])):
+        nextPointTmp = [[pointsO3[0][j], pointsO3[1][j]]]
+        #print(*nextPointTmp, sep=", ")
+        WO3[i][j] = geopy.distance.vincenty(pointTmp, nextPointTmp).km
+
+# Symmetric Matrix
+WO3 = WO3 + WO3.T
+#print(WO3)
+np.savetxt("/home/zap2x/Desktop/WO3.txt", WO3, fmt='%.2f')
+# Graph O3
+GO3 = graphs.Graph(WO3)
+
+# Graph Creation NO2
+pointsNO2 = [dataNO2['coordinates.longitude'].tolist(), dataNO2['coordinates.latitude'].tolist()]
+#print("\nCoordinates NO2")
+#print(pointsNO2)
+
+sizeNO2 = len(dataNO2)
+WNO2 = np.zeros((sizeNO2, sizeNO2))
+
+for i in range(0, len(pointsNO2[0])):
+    pointTmp = [pointsNO2[0][i], pointsNO2[1][i]]
+    #print(*pointTmp, sep=", ")
+    for j in range(i, len(pointsNO2[0])):
+        nextPointTmp = [[pointsNO2[0][j], pointsNO2[1][j]]]
+        #print(*nextPointTmp, sep=", ")
+        WNO2[i][j] = geopy.distance.vincenty(pointTmp, nextPointTmp).km
+
+# Symmetric Matrix
+WNO2 = WNO2 + WNO2.T
+#print(WNO2)
+# Graph NO2
+GNO2 = graphs.Graph(WNO2)
+
+# Graph Creation PM25
+pointsPM25 = [dataPM10['coordinates.longitude'].tolist(), dataPM10['coordinates.latitude'].tolist()]
+#print("\nCoordinates PM25")
+#print(pointsPM25)
+
+sizePM25 = len(dataPM10)
+WPM10 = np.zeros((sizePM25, sizePM25))
+
+for i in range(0, len(pointsPM25[0])):
+    pointTmp = [pointsPM25[0][i], pointsPM25[1][i]]
+    #print(*pointTmp, sep=", ")
+    for j in range(i, len(pointsPM25[0])):
+        nextPointTmp = [[pointsPM25[0][j], pointsPM25[1][j]]]
+        #print(*nextPointTmp, sep=", ")
+        WPM10[i][j] = geopy.distance.vincenty(pointTmp, nextPointTmp).km
+
+# Symmetric Matrix
+WPM10 = WPM10 + WPM10.T
+#print(WPM10)
+# Graph PM25
+GPM25 = graphs.Graph(WPM10)
