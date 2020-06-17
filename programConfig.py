a7e21d25c1575178e532f69f15554aac3be5720c
isalfati <ivansalfatift@gmail.com>
2020-06-17 17:02:58 +0200

    Results February

--- programConfig.py
+++ programConfig.py
@@ -34,15 +34,17 @@ selection = ["o3", "no2", "pm10"]
 
 # City & Date       # If city = BCN, month = March, the small dataset test will be used instead.
 city = "Barcelona"  # Right now, Barcelona is the only one possible.
+month = "February"   # January, February and March possible.
 year  = "2020"      # Only 2020 possible.
 
 # Parameter to analyze
+contaminant = "pm10" # any of the selection list.
 
 # Minimum amount of data for a station to be kept 0 < x < 1
 minPercentatge = 0.85
 
 # Maximum distance between stations
-#maxDistanceBetweenStations = 10 # km 
+maxDistanceBetweenStations = 20 # km 
 
 # Target Station Selector:
 # [0]: Station to reconstruct, [1]: Faulty Station
@@ -71,12 +73,8 @@ decimals = 4
 decimalsSparse = 3
 
 # Value of K to reconstruct
+VALUEK = 8
 
 # Error Percentage
-
-month = "January"   # January, February and March possible.
-contaminant = "pm10" # any of the selection list.
-maxDistanceBetweenStations = 20 # km 
-VALUEK = 8
 levelError = 0.20
 
